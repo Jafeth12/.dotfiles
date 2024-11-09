7ffcbacb36ec9577553ab6ffdbda6ff7ebd10f83
jafeth <jafethrafael12@gmail.com>
2024-11-09 14:10:09 +0100

    telescope lsp used

--- .config/nvim/lua/core/plugconfig/lsp_config.lua
+++ .config/nvim/lua/core/plugconfig/lsp_config.lua
@@ -15,13 +15,14 @@ require("mason-lspconfig").setup({
     }
 })
 
+local telbuiltin = require('telescope.builtin')
+
 local on_attach = function(_, _)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
     vim.keymap.set('n', '<leader>qf', vim.lsp.buf.code_action, {})
-
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+    vim.keymap.set('n', 'gd', telbuiltin.lsp_definitions, {})
+    vim.keymap.set('n', 'gi', telbuiltin.lsp_implementations, {})
+    vim.keymap.set('n', 'gr', telbuiltin.lsp_references, {})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
